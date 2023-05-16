dd8ccb230f49f2f5041cdb1ea8c71ab25785090a
Ismael Jimenez <ismadelic@Ismaels-MacBook-Pro.local>
2023-05-16 19:35:13 -0400

    added metric, costfunc, optimizer, and dataimports to driver

--- optiFit.py
+++ optiFit.py
@@ -2,26 +2,29 @@ import yaml
 import sys
 
 from funcFactory import FuncFactory
-# from metricFactory import MetricFactory
-# from costFunc import CostFunc
-# from optimizerFactory import OptimizerFactory
+from metricFactory import MetricFactory
+from CostFunction import CostFunction
+from OptimizationTechniqueFactory import OptimizationTechniqueFactory
+from dataImportExport import DataImportExport
 
 def main():
 
-    if len(sys.argv) != 2:
-        sys.exit("Must include YAML file name")
+    if len(sys.argv) != 3:
+        sys.exit("Must include YAML file name and data file")
 
-    file_name = sys.argv[1]
+    yml_file = sys.argv[1]
+    data_file = sys.argv[2]
 
-    with open(file_name, 'r') as fstream:
+    with open(yml_file, 'r') as fstream:
         dict = yaml.safe_load(fstream)
+    
+    dataImport = DataImportExport(data_file)
+    data = dataImport.import_data()
 
     function = FuncFactory.create(dict['function']['name'], **dict['function']['params'])
-
-# Will edit later
-    # metric = MetricFactory.create(dict['metric']['name'])
-    # costFunc = CostFunc(data, function, metric)
-    # optimizer = OptimizerFactory.create(dict['optimizer']['name'])
+    metric = MetricFactory.create(dict['metric']['name'])
+    costFunc = CostFunction(data, function, metric)
+    optimizer = OptimizationTechniqueFactory.create(dict['optimizer']['name'])
 
 
 main()
