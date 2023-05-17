0b3309ba835c1102f70467e77d8a056ef629fa77
Anush Musthyala <anushm360@gmail.com>
2023-05-17 14:40:42 -0400

    Resolved optiFit merge conflict mess with main-->driver

--- optiFit.py
+++ optiFit.py
@@ -10,15 +10,19 @@ def cost_function(params, model_func, metric_func, x, y_true):
     y_pred = model_func(params, x)
     return metric_func(y_pred, y_true)
 
+
 def main():
 
     if len(sys.argv) != 2:
         sys.exit("Must include YAML file name")
 
-    file_name = sys.argv[1]
-
-    with open(file_name, 'r') as fstream:
+    yml_file = sys.argv[1]
+    with open(yml_file, 'r') as fstream:
         dict = yaml.safe_load(fstream)
+    
+    data_file = dict['data']['name']
+    dataImport = DataImportExport(data_file)
+    data = dataImport.import_data()
 
     function = FuncFactory.create(dict['function']['name'], **dict['function']['params'])
 
@@ -36,6 +40,7 @@ def main():
     # metric = MetricFactory.create(dict['metric']['name'])
     # costFunc = CostFunc(data, function, metric)
     # optimizer = OptimizerFactory.create(dict['optimizer']['name'])
+
 
 
 main()
