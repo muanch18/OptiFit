d7d6280d12f6255fd3fe642acd297c277b1589f4
Anush Musthyala <anushm360@gmail.com>
2023-05-17 12:51:54 -0400

    driver changes

--- optiFit.py
+++ optiFit.py
@@ -1,10 +1,14 @@
 import yaml
 import sys
-
+import numpy as np
 from funcFactory import FuncFactory
-# from metricFactory import MetricFactory
-# from costFunc import CostFunc
-# from optimizerFactory import OptimizerFactory
+from metricFactory import MetricFactory
+from OptimizationTechniqueFactory import OptimizationTechniqueFactory
+from dataImportExport import DataImportExport
+
+def cost_function(params, model_func, metric_func, x, y_true):
+    y_pred = model_func(params, x)
+    return metric_func(y_pred, y_true)
 
 def main():
 
@@ -18,6 +22,16 @@ def main():
 
     function = FuncFactory.create(dict['function']['name'], **dict['function']['params'])
 
+    metric = MetricFactory.create(dict['metric']['name'])
+
+    data = DataImportExport.import_data(dict['data']['file_name'])
+
+    x = np.array(data['x'])
+    y_true = np.array(data['y'])
+
+    optimizer = OptimizationTechniqueFactory.create(dict['optimizer']['name'], cost_function, function, metric, x, y_true)
+
+    result = optimizer.optimize()
 # Will edit later
     # metric = MetricFactory.create(dict['metric']['name'])
     # costFunc = CostFunc(data, function, metric)
