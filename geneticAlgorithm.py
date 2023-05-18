c753bd82fd87db0237e4d923dfd9beb4c446dbb1
Ismael Jimenez <ismadelic@Ismaels-MacBook-Pro.local>
2023-05-18 16:07:32 -0400

    updated genetic algorithm to current format, DRAFT

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -2,10 +2,9 @@ from OptimizationTechnique import OptimizationTechnique
 from scipy.optimize import differential_evolution
 
 class GeneticAlgorithm(OptimizationTechnique):
-    def __init__(self, cost_function, bounds):
-        super().__init__(cost_function)
+    def __init__(self, bounds):
         self.bounds = bounds
 
-    def optimize(self, initial_parameters):
-        result = differential_evolution(self.cost_function, self.bounds)
+    def optimize(self, cost_function, initial_parameters):
+        result = differential_evolution(cost_function(initial_parameters), self.bounds)
         return result.x, result.fun
