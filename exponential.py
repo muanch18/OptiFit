a0dee934cf8895ecfaaed4070100f033e248e6db
Ismael Jimenez <ismadelic@Ismaels-MacBook-Pro.local>
2023-05-18 19:48:19 -0400

    modified exponential function

--- exponential.py
+++ exponential.py
@@ -1,8 +1,9 @@
 from funcFamily import FuncFamily
+import numpy as np
 
 class Exponential(FuncFamily):
     def _func(self, x):
-        return self.a * (self.b ** x)
+        return self.a * np.e**(self.b * x)
 
     def update_params(self, params):
         self.a = params[0]
