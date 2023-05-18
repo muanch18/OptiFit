98b182071e7eb6fdcdd0317ce4903a288b1ee46b
Ismael Jimenez <ismadelic@Ismaels-MacBook-Pro.local>
2023-05-18 11:23:48 -0400

    modified costFunction class included inputs,ouputs, func and metric as parameters

--- CostFunction.py
+++ CostFunction.py
@@ -1,8 +1,15 @@
-class CostFunction:
-    def __init__(self, y_true, y_pred, metric):
-        self.y_true = y_true
-        self.y_pred = y_pred
-        self.metric = metric
 
-    def cost(self):
-        return self.metric(self.y_true, self.y_pred)
+class CostFunction:
+    def __init__(self, inputs, outputs, func, metric):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.func = func
+        self.metric = metric
+    
+    def __call__(self, *params):
+        return self.costFunc(*params)
+    
+    def costFunc(self, *params):
+        self.func.update_params(*params)
+        y_pred = self.func(self.inputs)
+        return self.metric(y_pred, self.outputs)
