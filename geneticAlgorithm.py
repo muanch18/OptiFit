b0a7e26c7901c16ef90fccea12164c1a69f683d3
Ismael Jimenez <ismadelic@Ismaels-MacBook-Pro.local>
2023-05-18 17:06:09 -0400

    got the genetic algorithm to work

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -3,8 +3,10 @@ from scipy.optimize import differential_evolution
 
 class GeneticAlgorithm(OptimizationTechnique):
     def __init__(self, bounds):
-        self.bounds = bounds
+        self.bounds = []
+        for key in bounds:
+            self.bounds.append((bounds[key]['min'], bounds[key]['max']))
 
     def optimize(self, cost_function, initial_parameters):
-        result = differential_evolution(cost_function(initial_parameters), self.bounds)
-        return result.x, result.fun
+        result = differential_evolution(cost_function, self.bounds)
+        return result.x
